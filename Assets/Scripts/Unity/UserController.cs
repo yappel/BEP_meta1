15a80d252e86478ff908b121c5288758d7bd04d4
lkrombeen <larskrombeen@gmail.com>
2016-05-02 21:34:23 +0200

    Base for the script that can be used in unity that will be coupled to the MetaWorld. Methods calls are there, just need the logic.

---
+++ Assets/Scripts/Unity/UserController.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="UserController.cs" company="Delft Universite of Technology">
+// Copyright (c) Delft University of Technology. All rights reserved.
+// </copyright>
+
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using Meta;
+using UnityEngine;
+
+[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1400:AccessModifierMustBeDeclared", Justification = "Reviewed.")]
+
+/// <summary>
+///  UserController keeps track of the user and its behavior.
+/// </summary>
+public class UserController : MonoBehaviour
+{
+    /// <summary>
+    ///   Keeps track of the user location and calculates the new one on update.
+    /// </summary>
+    private PredictionWeightBuffer predictionWeightBuffer;
+    
+    /// <summary>
+    ///   Method called when creating a UserController.
+    /// </summary>
+    void Start()
+    {
+        this.predictionWeightBuffer = new PredictionWeightBuffer();
+    }
+
+    /// <summary>
+    ///   Method calles on every frame.
+    /// </summary>
+    void Update()
+    {
+        IRVectorTransform newLocation = this.predictionWeightBuffer.predictLocation(null);
+        transform.position = new Vector3(newLocation.GetPosition().GetX(), newLocation.GetPosition().GetY(), newLocation.GetPosition().GetZ());
+        transform.rotation = new Quaternion(newLocation.GetRotation().GetX(), newLocation.GetRotation().GetY(), newLocation.GetRotation().GetZ(), 1);
+    }
+}
--- Assets/Scripts/UserLocalisation/PredictionWeightBuffer.cs
+++ Assets/Scripts/UserLocalisation/PredictionWeightBuffer.cs
@@ -13,10 +13,34 @@ public class PredictionWeightBuffer
     private const float WEIGHTMARKER = 1;
 
     /// <summary>
+    ///   The used user localization technique.
+    /// </summary>
+    private AbstractUserLocalisation userLocalisation;
+
+    /// <summary>
+    ///   All mapped Markers.
+    /// </summary>
+    private MarkerLocations markerLocations;
+
+    /// <summary>
     ///   Initializes a new instance of the PredictionWeightBuffer class.
     /// </summary>
     public PredictionWeightBuffer()
     {
-        // TODO initialise the prediction array and pass it to an AbstractUserLocalisation class.
+        // TODO userLocalisation = new ...
+        this.markerLocations = new MarkerLocations();
+    }
+
+    /// <summary>
+    ///   Initializes a new instance of the PredictionWeightBuffer class.
+    /// </summary>
+    /// <returns>IRVectorTransform the predicted location and rotation</returns>
+    /// <param name="visibleMarkerIds">Array of the ids of the visible Markers.</param>
+    public IRVectorTransform PredictLocation(int[] visibleMarkerIds)
+    {
+        // Call to abstractuserlocalisation
+        // Each visibleMarkerId gets loaded from markerLocations
+        // We need a way to also give the position + rotation from Unity (IRVectorTransform class can be used)
+        return new IRVectorTransform(new IRVector3(0, 0, 0), new IRVector3(0, 0, 0));
     }
 }
