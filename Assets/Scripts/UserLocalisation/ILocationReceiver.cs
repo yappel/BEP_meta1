da0908753259119dc7d9d4951ccf68f6133577c3
Yoeri Appel <y.a.appel@student.tudelft.nl>
2016-05-03 22:09:40 +0200

    Created location receiver interface.

---
+++ Assets/Scripts/UserLocalisation/ILocationReceiver.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ILocationReceiver.cs" company="Delft University of Technology">
+// Copyright (c) Delft University of Technology. All rights reserved.
+// </copyright>
+
+namespace Assets.Scripts.UserLocalisation
+{
+    using Assets.Scripts.Inputsensors;
+
+    /// <summary>
+    /// A location receiver is able to register a location source
+    /// </summary>
+    public interface ILocationReceiver
+    {
+        /// <summary>
+        /// Registers a location source.
+        /// </summary>
+        /// <param name="source">The location source to register</param>
+        void RegisterLocationReceiver(ILocationSource source);
+    }
+}
