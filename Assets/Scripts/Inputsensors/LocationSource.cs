23eff5475e554e5a460ad80dc59c6384f0ded603
Yoeri Appel <y.a.appel@student.tudelft.nl>
2016-05-03 17:59:11 +0200

    Created interfaces for the LocationSources and MotionSources.

---
+++ Assets/Scripts/Inputsensors/LocationSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Inputsensors
+{
+    /// <summary>
+    ///   A LocationSource provides information about the location of the user.
+    /// </summary>
+    public interface LocationSource
+    {
+        /// <summary>
+        /// Gets the location vector.
+        /// </summary>
+        /// <returns>A SensorVector3 containing the location in the xyz dimensions in meters and a deviation to specify the certainty of this data</returns>
+        SensorVector3 GetLocation();
+    }
+}
---
+++ Assets/Scripts/Inputsensors/MotionSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Inputsensors
+{
+    /// <summary>
+    ///   A MotionSource provides information about the motion of the user.
+    /// </summary>
+    public interface MotionSource
+    {
+        /// <summary>
+        /// Gets the motion vector.
+        /// </summary>
+        /// <returns>A SensorVector3 containing the motion in the xyz dimensions in m/s and a deviation to specify the certainty of this data</returns>
+        SensorVector3 GetMotion();
+    }
+}
