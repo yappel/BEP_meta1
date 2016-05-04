f7353362d00c4c2697aad4d6f30ae28a44390238
lkrombeen <larskrombeen@gmail.com>
2016-05-04 18:08:43 +0200

    Adjusted the calculation get the absolute location from relative position to markers.

--- Assets/Scripts/Inputsensors/MarkerSensor.cs
+++ Assets/Scripts/Inputsensors/MarkerSensor.cs
@@ -9,12 +9,17 @@ using Assets.Scripts.Inputsensors;
 /// <summary>
 ///  This class keeps track of the location based on Markers.
 /// </summary>
-public class MarkerSensor : ILocationSource
+public class MarkerSensor : ILocationSource, IRotationSource
 {
     /// <summary>
     ///   The predicted locations.
     /// </summary>
     private List<SensorVector3> locations;
+
+    /// <summary>
+    ///   The predicted rotations.
+    /// </summary>
+    private List<SensorVector3> rotations;
 
     /// <summary>
     ///   Gets or sets the MarkerLocations.
@@ -39,19 +44,30 @@ public class MarkerSensor : ILocationSource
     }
 
     /// <summary>
+    ///   Get the rotations based on the visible markers.
+    /// </summary>
+    /// <returns>List of SensorVector3</returns>
+    public List<SensorVector3> GetRotations()
+    {
+        return this.rotations;
+    }
+
+    /// <summary>
     ///   Update the locations derived from Markers.
     /// </summary>
     /// <param name="visibleMarkerIds">Dictionary of the ids and transforms ((x,y,z), (pitch, yaw, rotation) in degrees) of the visible Markers.</param>
     public void UpdateLocations(Dictionary<int, IRVectorTransform> visibleMarkerIds)
     {
         this.locations = new List<SensorVector3>(visibleMarkerIds.Count);
+        this.rotations = new List<SensorVector3>(visibleMarkerIds.Count);
         foreach (KeyValuePair<int, IRVectorTransform> pair in visibleMarkerIds)
         {
             try
             {
                 Marker currentMarker = this.MarkerLocations.GetMarker(pair.Key);
-                IRVector3 location = Positioning.GetPosition(currentMarker, pair.Value);
-                this.locations.Add(new SensorVector3(location.GetX(), location.GetY(), location.GetZ(), 1));
+                IRVectorTransform location = Positioning.GetPosition(currentMarker, pair.Value);
+                this.locations.Add(new SensorVector3(location.GetPosition().GetX(), location.GetPosition().GetY(), location.GetPosition().GetZ(), 1));
+                this.rotations.Add(new SensorVector3(location.GetRotation().GetX(), location.GetRotation().GetY(), location.GetRotation().GetZ(), 1));
             }
             catch (UnallocatedMarkerException e)
             {
--- Assets/Scripts/Utils/Positioning.cs
+++ Assets/Scripts/Utils/Positioning.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 ///   Utility class to calculate the absolute position of a relative position.
 /// </summary>
 public static class Positioning
-{
+{ 
     /// <summary>
     ///   Convert a radian to a degree angle.
     /// </summary>
@@ -21,15 +21,16 @@ public static class Positioning
     /// <param name="absoluteLocation">The known absolute position.</param>
     /// <param name="relativeLocation">The position relative to the absolute location.</param>
     /// <returns>The location of the calculated absolute position.</returns>
-    public static IRVector3 GetPosition(Marker absoluteLocation, IRVectorTransform relativeLocation)
+    public static IRVectorTransform GetPosition(Marker absoluteLocation, IRVectorTransform relativeLocation)
     {
         IRVector3 absolutePosition = absoluteLocation.GetPosition();
         IRVector3 absoluteRotation = absoluteLocation.GetRotation();
         IRVector3 relativeRotation = relativeLocation.GetRotation();
         float distance = CalculateDistance(absolutePosition, relativeLocation.GetPosition());
-        IRVector3 newLocation = CalculatePosition(absolutePosition, absoluteRotation, relativeRotation, distance);
+        IRVectorTransform newLocation = CalculatePosition(absolutePosition, absoluteRotation, relativeRotation, distance);
 
-        Debug.Log("X:" + newLocation.GetX() + ", Y:" + newLocation.GetY() + ", Z:" + newLocation.GetZ());
+        Debug.Log("ROTATION--- X:" + newLocation.GetRotation().GetX() + ", Y:" + newLocation.GetRotation().GetY() + ", Z:" + newLocation.GetRotation().GetZ());
+        Debug.Log("POSITION--- X:" + newLocation.GetPosition().GetX() + ", Y:" + newLocation.GetPosition().GetY() + ", Z:" + newLocation.GetPosition().GetZ());
 
         return newLocation;
     }
@@ -56,14 +57,19 @@ public static class Positioning
     /// <param name="absoluteRotation">Rotation of the known point</param>
     /// <param name="relativeRotation">Rotation of the relative point</param>
     /// <param name="distance">Distance to the absolute point as seen from the relative point</param>
-    /// <returns>3D euclidean distance</returns>
-    private static IRVector3 CalculatePosition(
+    /// <returns>Location and rotation of the relatively given location</returns>
+    private static IRVectorTransform CalculatePosition(
         IRVector3 absolutePosition, IRVector3 absoluteRotation, IRVector3 relativeRotation, float distance)
     {
-        float newX = (float)Math.Sin(relativeRotation.GetY() * toRadian) * distance;
-        float newY = (float)Math.Cos(relativeRotation.GetX() * toRadian) * distance;
-        float newZ = (float)Math.Cos(relativeRotation.GetY() * toRadian) * distance;
+        IRVector3 rotation = new IRVector3(
+            relativeRotation.GetX() - absoluteRotation.GetX(),
+            relativeRotation.GetY() - absoluteRotation.GetY(),
+            relativeRotation.GetZ() - absoluteRotation.GetZ());
+        IRVector3 position = new IRVector3(
+            absolutePosition.GetX() + (float)(distance * Math.Cos(rotation.GetZ() * toRadian) * Math.Sin(rotation.GetY() * toRadian)), 
+            absolutePosition.GetY() + (float)(distance * Math.Sin(rotation.GetZ() * toRadian)), 
+            absolutePosition.GetZ() + (float)(distance * Math.Cos(rotation.GetZ() * toRadian) * Math.Cos(rotation.GetY() * toRadian)));
 
-        return new IRVector3(newX, newY, newZ);
+        return new IRVectorTransform(position, rotation);
     }
 }
