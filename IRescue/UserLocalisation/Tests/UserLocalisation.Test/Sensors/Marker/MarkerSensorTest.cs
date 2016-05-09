a74a3d0a2d3aa877022b2f7857b2c36c594efd6b
lkrombeen <larskrombeen@gmail.com>
2016-05-09 19:46:07 +0200

    Test setup for userlocalisation

---
+++ IRescue/UserLocalisation/Tests/UserLocalisation.Test/Sensors/Marker/MarkerSensorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UserLocalisation.Test.Sensors.Marker
+{
+    [TestFixture]
+    class MarkerSensorTest
+    {
+        [Test]
+        public void TestTest()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
