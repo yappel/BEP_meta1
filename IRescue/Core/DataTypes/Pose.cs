545e5475c42e5c215d0995501adc29a3fb7dd943
Yoeri Appel <y.a.appel@student.tudelft.nl>
2016-05-09 12:39:39 +0200

    Added the Pose class.

---
+++ IRescue/Core/DataTypes/Pose.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Pose.cs" company="Delft University of Technology">
+// Copyright (c) Delft University of Technology. All rights reserved.
+// </copyright>
+namespace IRescue.Core.DataTypes
+{
+    /// <summary>
+    /// A pose of an object or person. Contains the orientation and position.
+    /// </summary>
+    public class Pose
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pose"/> class.
+        /// </summary>
+        /// <param name="position">The position of the object/person in cartesian coordinates</param>
+        /// <param name="orientation">The orientation of the object/person in degrees</param>
+        public Pose(Vector3 position, Vector3 orientation)
+        {
+            this.Position = position;
+            this.Orientation = orientation;
+        }
+
+        /// <summary>
+        /// Gets or sets the position of the pose.
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position of the pose.
+        /// </summary>
+        public Vector3 Orientation { get; set; }
+    }
+}
