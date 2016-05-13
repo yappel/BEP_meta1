8c0330f38dd94a0831986526b822eaa0212b68d3
lkrombeen <larskrombeen@gmail.com>
2016-05-13 13:27:29 +0200

    MarkerSensor now buffers its data.

--- IRescue/UserLocalisation/Sensors/Marker/MarkerSensor.cs
+++ IRescue/UserLocalisation/Sensors/Marker/MarkerSensor.cs
@@ -20,20 +20,30 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         private readonly float standardDeviation;
 
         /// <summary>
-        ///   The predicted locations.
-        /// </summary>
-        private List<Measurement<Vector3>> positions;
-
-        /// <summary>
-        ///   The predicted rotations.
-        /// </summary>
-        private List<Measurement<Vector3>> orientations;
-
-        /// <summary>
         ///  The MarkerLocations.
         /// </summary>
         private MarkerLocations markerLocations;
 
+        /// <summary>
+        ///   The predicted rotations.
+        /// </summary>
+        private Measurement<Vector3>[] orientations;
+
+        /// <summary>
+        ///   The predicted locations.
+        /// </summary>
+        private Measurement<Vector3>[] positions;
+
+        /// <summary>
+        /// Current index pointer;
+        /// </summary>
+        private int pointer = 0;
+
+        /// <summary>
+        /// Length of the buffer.
+        /// </summary>
+        private int bufferLength = 30;
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="MarkerSensor"/> class.
         /// </summary>
@@ -43,7 +53,26 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         {
             this.standardDeviation = standardDeviation;
             this.markerLocations = new MarkerLocations(path);
+            this.orientations = new Measurement<Vector3>[this.bufferLength];
+            this.positions = new Measurement<Vector3>[this.bufferLength];
+            this.Measurements = 0;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MarkerSensor"/> class with a given buffer size.
+        /// </summary>
+        /// <param name="standardDeviation">the standard deviation</param>
+        /// <param name="path">url to the xml file</param>
+        /// <param name="bufferLength">Length of the buffer</param>
+        public MarkerSensor(float standardDeviation, string path, int bufferLength) : this(standardDeviation, path)
+        {
+            this.bufferLength = bufferLength;
+        }
+
+        /// <summary>
+        /// Gets the amount of measurements.
+        /// </summary>
+        public int Measurements { get; private set; }
 
         /// <summary>
         ///   Update the locations derived from Markers.
@@ -52,16 +81,16 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         public void UpdateLocations(Dictionary<int, Pose> visibleMarkerIds)
         {
             long timeStamp = StopwatchSingleton.Time;
-            this.positions = new List<Measurement<Vector3>>(visibleMarkerIds.Count);
-            this.orientations = new List<Measurement<Vector3>>(visibleMarkerIds.Count);
             foreach (KeyValuePair<int, Pose> pair in visibleMarkerIds)
             {
                 try
                 {
                     Pose currentMarkerPose = this.markerLocations.GetMarker(pair.Key);
                     Pose location = AbRelPositioning.GetLocation(currentMarkerPose, pair.Value);
-                    this.positions.Add(new Measurement<Vector3>(location.Position, this.standardDeviation, timeStamp));
-                    this.orientations.Add(new Measurement<Vector3>(location.Orientation, this.standardDeviation, timeStamp));
+                    this.positions[this.pointer] = new Measurement<Vector3>(location.Position, this.standardDeviation, timeStamp);
+                    this.orientations[this.pointer] = new Measurement<Vector3>(location.Orientation, this.standardDeviation, timeStamp);
+                    this.pointer = this.pointer >= this.bufferLength - 1 ? 0 : this.pointer + 1;
+                    this.Measurements++;
                 }
                 catch (UnallocatedMarkerException e)
                 {
@@ -76,7 +105,7 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         /// <returns>The orientation <see cref="Measurement{T}"/> with time stamp and standard deviation. Null if no data was found</returns>
         public Measurement<Vector3> GetLastOrientation()
         {
-            return this.orientations.Count > 0 ? this.orientations[this.orientations.Count - 1] : null;
+            return this.orientations.Length > 0 ? this.orientations[(this.pointer - 1) % this.bufferLength] : null;
         }
 
         /// <summary>
@@ -106,7 +135,7 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         /// <returns>A list of all <see cref="Measurement{T}"/> and their time stamps and standard deviations.</returns>
         public List<Measurement<Vector3>> GetAllOrientations()
         {
-            return this.orientations;
+            return this.ArrayToList(this.orientations);
         }
 
         /// <summary>
@@ -115,7 +144,7 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         /// <returns>The last measured acceleration with time stamp and standard deviation. Null if no data was found</returns>
         public Measurement<Vector3> GetLastPosition()
         {
-            return this.positions.Count > 0 ? this.positions[this.positions.Count - 1] : null;
+            return this.positions.Length > 0 ? this.positions[(this.pointer - 1) % this.bufferLength] : null;
         }
 
         /// <summary>
@@ -145,18 +174,35 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         /// <returns>A list of all <see cref="Measurement{T}"/> and their time stamps and standard deviations.</returns>
         public List<Measurement<Vector3>> GetAllPositions()
         {
-            return this.positions;
+            return this.ArrayToList(this.positions);
         }
 
         /// <summary>
-        /// Get a <see cref="Measurement{T}"/> with the give timestamp.
+        /// Convert an array to a list
         /// </summary>
-        /// <param name="measurements">The list of <see cref="Measurement{T}"/></param>
+        /// <param name="measurements">the array</param>
+        /// <returns>the list in the correct order of measurements</returns>
+        private List<Measurement<Vector3>> ArrayToList(Measurement<Vector3>[] measurements)
+        {
+            int length = this.GetIterationLength(measurements);
+            List<Measurement<Vector3>> res = new List<Measurement<Vector3>>(length);
+            for (int i = 0; i < length; i++)
+            {
+                res.Add(measurements[this.GetIndex(i)]);
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Get a <see cref="Measurement{T}"/> with the give timestamp. This is NOT the recommended method since multiple markers can be seen in 1 iteration.
+        /// </summary>
+        /// <param name="measurements">The array of <see cref="Measurement{T}"/></param>
         /// <param name="timeStamp">The timestamp</param>
         /// <returns>The <see cref="Measurement{T}"/>. Null if no data was measured at this timestamp</returns>
-        private Measurement<Vector3> GetTimeStampMeasurement(List<Measurement<Vector3>> measurements, long timeStamp)
+        private Measurement<Vector3> GetTimeStampMeasurement(Measurement<Vector3>[] measurements, long timeStamp)
         {
-            for (int i = 0; i < measurements.Count; i++)
+            for (int i = 0; i < this.GetIterationLength(measurements); i++)
             {
                 if (measurements[i].TimeStamp == timeStamp)
                 {
@@ -174,19 +220,42 @@ namespace IRescue.UserLocalisation.Sensors.Marker
         /// <param name="startTimeStamp">The start timestamp</param>
         /// <param name="endTimeStamp">The end timestamp</param>
         /// <returns>List of <see cref="Measurement{T}"/> between the timestamps</returns>
-        private List<Measurement<Vector3>> GetTimeStampMeasurements(
-            List<Measurement<Vector3>> measurements, long startTimeStamp, long endTimeStamp)
+        private List<Measurement<Vector3>> GetTimeStampMeasurements(Measurement<Vector3>[] measurements, long startTimeStamp, long endTimeStamp)
         {
+            int length = this.GetIterationLength(measurements);
             List<Measurement<Vector3>> timeMeasurements = new List<Measurement<Vector3>>();
-            for (int i = 0; i < measurements.Count; i++)
+            for (int i = 0; i < length; i++)
             {
-                if (measurements[i].TimeStamp >= startTimeStamp && measurements[i].TimeStamp <= endTimeStamp)
+                int index = this.GetIndex(i);
+
+                Console.WriteLine(this.GetIndex(i));
+                if (measurements[index].TimeStamp >= startTimeStamp && measurements[index].TimeStamp <= endTimeStamp)
                 {
-                    timeMeasurements.Add(measurements[i]);
+                    timeMeasurements.Add(measurements[index]);
                 }
             }
 
             return timeMeasurements;
+        }
+
+        /// <summary>
+        /// Return the previous data point.
+        /// </summary>
+        /// <param name="i">iterator value</param>
+        /// <returns>measurement i iterations ago</returns>
+        private int GetIndex(int i)
+        {
+            return (((this.pointer - i) % this.bufferLength) + this.bufferLength - 1) % this.bufferLength;
+        }
+
+        /// <summary>
+        /// Return the length of the data size.
+        /// </summary>
+        /// <param name="measurements">the measurements array</param>
+        /// <returns>the length</returns>
+        private int GetIterationLength(Measurement<Vector3>[] measurements)
+        {
+            return Math.Min(this.bufferLength, this.Measurements);
         }
     }
 }
