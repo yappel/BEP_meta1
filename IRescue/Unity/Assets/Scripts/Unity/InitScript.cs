0da61af4ddff7bd407a0f7dd9d57751eed747b60
lkrombeen <larskrombeen@gmail.com>
2016-05-24 15:16:45 +0200

    Bigger groundplane!

--- IRescue/Unity/Assets/Scripts/Unity/InitScript.cs
+++ IRescue/Unity/Assets/Scripts/Unity/InitScript.cs
@@ -40,7 +40,7 @@ namespace Assets.Scripts.Unity
             this.InitControllers(coupler);
             this.InitUser(coupler.GetLocalizer());
             this.InitMarker();
-            this.InitPlanes(20, 20);
+            this.InitPlanes(200, 200);
         }
 
         /// <summary>
