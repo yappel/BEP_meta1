9951443009bdccb4fab664b45f12535bd5425243
lkrombeen <larskrombeen@gmail.com>
2016-05-25 14:47:48 +0200

    You can save your game

--- IRescue/Unity/Assets/Scripts/Unity/ObjectPlacing/States/SaveState.cs
+++ IRescue/Unity/Assets/Scripts/Unity/ObjectPlacing/States/SaveState.cs
@@ -2,32 +2,35 @@
 // Copyright (c) Delft University of Technology. All rights reserved.
 // </copyright>
 
-using System;
-
 namespace Assets.Scripts.Unity.ObjectPlacing.States
 {
+    using System;
+    using UnityEngine;
+    using System.Xml;
+    using UnityEngine.UI;
+
     /// <summary>
     /// State when saving the game.
     /// </summary>
     public class SaveState : AbstractState
     {
+        private const string SaveFile = "Saves/";
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SaveState"/> class.
         /// </summary>
         /// <param name="stateContext">The class that keeps track of the current active state</param>
         public SaveState(StateContext stateContext) : base(stateContext)
         {
-            if (this.StateContext != null)
+            if (this.StateContext.SaveFilePath != null)
             {
                 this.SaveGame(this.StateContext.SaveFilePath);
                 this.StateContext.SetState(new NeutralState(this.StateContext));
             }
             else
             {
-                this.StateContext.Buttons.BackButton.SetActive(true);
-                this.StateContext.Buttons.SaveButton.SetActive(true);
-                this.StateContext.Buttons.TextInput.SetActive(true);
-                // TODO set textinput text
+                this.StateContext.Buttons.SetActive(new GameObject[] { this.StateContext.Buttons.BackButton, this.StateContext.Buttons.SaveButton, this.StateContext.Buttons.TextInput });
+                this.StateContext.Buttons.TextInput.GetComponentInChildren<Text>().text = DateTime.Now.ToString("yyyyMMddHHmm");
             }
         }
 
@@ -44,15 +47,16 @@ namespace Assets.Scripts.Unity.ObjectPlacing.States
         /// </summary>
         public override void OnSaveButton()
         {
-            // TODO save all buildings to an xml
             try
             {
-                this.SaveGame("SOMETHING");
+                string saveName = StateContext.Buttons.TextInput.GetComponentInChildren<Text>().text.Replace(" ", "").Replace("\\", "").Replace("/", "");
+                this.SaveGame(SaveFile + saveName + ".xml");
+                this.StateContext.SaveFilePath = saveName;
                 this.StateContext.SetState(new NeutralState(this.StateContext));
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                // TODO show message
+                this.StateContext.Buttons.TextInput.GetComponentInChildren<Text>().text = "Invalid name, try using a different one!";
             }
         }
 
@@ -62,11 +66,60 @@ namespace Assets.Scripts.Unity.ObjectPlacing.States
         /// <param name="path"></param>
         private void SaveGame(string path)
         {
-            //TODO
+            XmlTextWriter writer = new XmlTextWriter(path, System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("objects");
+            foreach (Transform building in GameObject.FindObjectOfType<GroundPlane>().transform)
+            {
+                this.WriteObject(writer, building.name.Replace("(clone)", "").Trim(), building.localPosition, building.localEulerAngles);
+            }
 
-
-
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
             this.StateContext.SaveFilePath = path;
+        }
+
+        /// <summary>
+        /// Write an object to the xml file.
+        /// </summary>
+        /// <param name="writer">the xml writer</param>
+        /// <param name="objectName">name of the prefab name that is the origin of the game object</param>
+        /// <param name="position">the position of the object</param>
+        /// <param name="orientation">the orientation for the object</param>
+        private void WriteObject(XmlTextWriter writer, string objectName, Vector3 position, Vector3 orientation)
+        {
+            writer.WriteStartElement("object");
+            writer.WriteStartElement("mesh");
+            writer.WriteString(objectName);
+            writer.WriteEndElement();
+            writer.WriteStartElement("position");
+            this.WriteVector(writer, position);
+            writer.WriteEndElement();
+            writer.WriteStartElement("orientation");
+            this.WriteVector(writer, orientation);
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+        }
+
+        /// <summary>
+        /// Write the vector of the object to the xml file
+        /// </summary>
+        /// <param name="writer">the xml writer</param>
+        /// <param name="vector">the vector to write to the xml</param>
+        private void WriteVector(XmlTextWriter writer, Vector3 vector)
+        {
+            writer.WriteStartElement("x");
+            writer.WriteString(vector.x.ToString());
+            writer.WriteEndElement();
+            writer.WriteStartElement("y");
+            writer.WriteString(vector.y.ToString());
+            writer.WriteEndElement();
+            writer.WriteStartElement("z");
+            writer.WriteString(vector.z.ToString());
+            writer.WriteEndElement();
         }
     }
 }
