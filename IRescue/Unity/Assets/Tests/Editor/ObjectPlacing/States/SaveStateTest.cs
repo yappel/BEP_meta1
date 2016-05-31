5c40db22d8feea8f40953f7923468a62c6e57843
lkrombeen <larskrombeen@gmail.com>
2016-05-31 09:42:46 +0200

    Added empty test for classes to add

---
+++ IRescue/Unity/Assets/Tests/Editor/ObjectPlacing/States/SaveStateTest.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="SaveStateTest.cs" company="Delft University of Technology">
+// Copyright (c) Delft University of Technology. All rights reserved.
+// </copyright>
+
+using Assets.Scripts.Unity.ObjectPlacing.States;
+using NUnit.Framework;
+
+/// <summary>
+/// Tests for <see cref="SaveState"/>
+/// </summary>
+public class SaveStateTest
+{
+    /// <summary>
+    /// Test so it shows up in unity tests
+    /// </summary>
+    [Test]
+    public void EmptyTest()
+    {
+        return;
+    }
+}
