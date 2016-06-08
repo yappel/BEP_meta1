80bf6d06823888a70e539437de7c2777ad7d3278
lkrombeen <larskrombeen@gmail.com>
2016-06-08 12:55:12 +0200

    Debug option

--- IRescue/Unity/Assets/Scripts/Unity/GroundPlane.cs
+++ IRescue/Unity/Assets/Scripts/Unity/GroundPlane.cs
@@ -18,11 +18,20 @@ namespace Assets.Scripts.Unity
         /// <param name="z">depth of the ground plane</param>
         public void Init(float x, float z)
         {
+            bool debug = false;
             this.gameObject.name = "GroundPlane";
             this.gameObject.transform.position = new Vector3(0, -1.6f, 0);
             // TODO change this, but should be the same for all 3 values else the scaling gets weird.
             this.gameObject.transform.localScale = new Vector3(x, x, x);
-            this.gameObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/grid");
+            if (debug)
+            {
+                this.gameObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/grid");
+            }
+            else
+            {
+                this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+
             this.gameObject.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(10 * x, 10 * x);
         }
     }
