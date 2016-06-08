cdfd98e3a66403c1a26c78725ad96bd753005262
lkrombeen <larskrombeen@gmail.com>
2016-06-08 15:10:06 +0200

    Removed the C# waterlevel tracker to make room for the c++ tracker, created the water level controller code without the call to the c++ code, rest is there.

--- IRescue/Unity/Assets/Scripts/Unity/WaterTracking/WaterLevelController.cs
+++ IRescue/Unity/Assets/Scripts/Unity/WaterTracking/WaterLevelController.cs
@@ -4,9 +4,9 @@
 
 namespace Assets.Scripts.Unity
 {
-    using Meta;
+    using IRescue.Core.Utils;
+    using System;
     using UnityEngine;
-    using WaterLevelTracking;
 
     /// <summary>
     /// Keep track of the water level
@@ -19,14 +19,34 @@ namespace Assets.Scripts.Unity
         private const int SourceDevice = 0;
 
         /// <summary>
+        /// Amount of milliseconds before the water level should be calculated
+        /// </summary>
+        private const long IntervalTime = 1000;
+
+        /// <summary>
+        /// Amount of measurements to buffer
+        /// </summary>
+        private const int BufferSize = 10;
+
+        /// <summary>
+        /// The timestamp in milliseconds of the previous calculation
+        /// </summary>
+        private long previousCalculation = 0;
+
+        /// <summary>
         /// The water plane game object which will rise to occlude objects
         /// </summary>
         private GameObject waterPlane;
 
         /// <summary>
-        /// The water level tracker which calculates the water level
+        /// Array of the returned water level calculations
         /// </summary>
-        private WaterLevelTracker waterLevelTracker;
+        private float[] measurements;
+
+        /// <summary>
+        /// Current pointer to the measurements array
+        /// </summary>
+        private int pointer = 0;
 
         /// <summary>
         /// Initializes the water plane
@@ -35,13 +55,13 @@ namespace Assets.Scripts.Unity
         /// <param name="z">the depth of the plane</param>
         public void Init(float x, float z)
         {
-            this.waterLevelTracker = new WaterLevelTracker();
             this.waterPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
             this.waterPlane.name = "WaterPlane";
             this.waterPlane.transform.position = new Vector3(x * 5, -10, z * 5);
             this.waterPlane.transform.localScale = new Vector3(x, 1, z);
             this.waterPlane.GetComponent<MeshRenderer>().material.shader = Shader.Find("Masked/Mask");
             this.waterPlane.GetComponent<MeshRenderer>().material.renderQueue = 2990;
+            this.measurements = new float[BufferSize];
         }
 
         /// <summary>
@@ -49,15 +69,28 @@ namespace Assets.Scripts.Unity
         /// </summary>
         public void Update()
         {
-            /*
-             * ts: long
-             * Foreach visible marker
-             *  Get camera position (projection of xyz)
-             *  Get rotation (z)
-             *  waterLevelTracker calculate level (camerafeed, markerlocation, markersize, rotation, ts)
-             *  
-             * this.gameObject.position = new Vector3(this.gameObject.position.x, waterLevelTracker.GetWaterLevel(), this.gameObject.position.z)
-             */
+            if (StopwatchSingleton.Time - this.previousCalculation > IntervalTime)
+            {
+                this.previousCalculation = StopwatchSingleton.Time;
+                // TODO for each visible marker
+                //      double waterLevel = WaterLevelTracker::CalculateWaterLevel(input);
+                //      if (waterLevel > 0) {
+                //          this.measurements[pointer % BufferSize] = waterLevel;
+                //          this.pointer++;
+                //      }
+
+                this.SetWaterPositionY();
+            }
+        }
+
+        /// <summary>
+        /// Change the height of the water plane
+        /// </summary>
+        private void SetWaterPositionY()
+        {
+            float[] sorted = this.measurements;
+            Array.Sort(sorted);
+            this.waterPlane.transform.position = new Vector3(this.waterPlane.transform.position.x, sorted[sorted.Length / 2], this.waterPlane.transform.position.z);
         }
     }
 }
